d715fb15c04af986f63ec041b3e85343f4a6c2ee
fernandajaviera <fernandajaviera64@gmail.com>
2021-11-15 21:19:17 -0300

    controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'biblioteca/index'
+  get 'biblioteca/view'
+  get 'biblioteca/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
